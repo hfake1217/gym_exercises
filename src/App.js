c17714e48e947df77bf8cd2500b0b4b40dfdf1fb
Hayley Fake <hayleyfake@comcast.net>
2023-06-08 12:58:48 -0400

    add footer and import the new pages to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react'
-import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
+
+import './App.css';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
@@ -11,6 +16,7 @@ const App = () => {
             <Route path="/" element={<Home />} />
             <Route path="/exercise/:id" element={<ExerciseDetail />} />
         </Routes>
+        <Footer />
     </Box>
   )
 }
