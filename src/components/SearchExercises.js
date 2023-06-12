967d6ee290a4b18c8396ac32dddb5f303593d455
Hayley Fake <hayleyfake@comcast.net>
2023-06-12 13:13:01 -0400

    pass props and add state to home page

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -5,7 +5,7 @@ import { exerciseOptions, fetchData } from '../utils/fetchData';
 import HorizontalScrollbar from './HorizontalScrollbar';
 
 
-const SearchExercises = () => {
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
   const [search, setSearch] = useState('')
   const [exercises, setExercises] = useState('')
   const [bodyParts, setBodyParts] = useState('')
@@ -69,7 +69,7 @@ const SearchExercises = () => {
         </Button>
       </Box>
       <Box sx={{ position: 'relative', width: '100%', p: '20px'}}>
-          <HorizontalScrollbar data={bodyParts}/>
+          <HorizontalScrollbar data={bodyParts} bodyPart={bodyPart} setBodyPart={setBodyPart}/>
       </Box>
     </Stack>
   )
