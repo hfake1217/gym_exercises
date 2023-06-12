1312f7ef896e33e457a6cd86bc7536ab4b5545b2
Hayley Fake <hayleyfake@comcast.net>
2023-06-12 12:49:49 -0400

    add useEffect fcn and scrollbar box

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -7,6 +7,16 @@ import { exerciseOptions, fetchData } from '../utils/fetchData';
 const SearchExercises = () => {
   const [search, setSearch] = useState('')
   const [exercises, setExercises] = useState('')
+  const [bodyParts, setBodyParts] = useState('')
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
+
+      setBodyParts(['all', ...bodyPartsData]);
+    }
+    fetchExercisesData();
+  }, [])
 
   const handleSearch = async () => {
     if(search) {
@@ -56,6 +66,9 @@ const SearchExercises = () => {
         >
           Search
         </Button>
+      </Box>
+      <Box sx={{ position: 'relative', width: '100%', p: '20px'}}>
+          <HorizontalScrollbar data={bodyParts}/>
       </Box>
     </Stack>
   )
