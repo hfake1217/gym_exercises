20e8a1129c2cfd934ab8e6bcd43955e8ded1cb3a
Hayley Fake <hayleyfake@comcast.net>
2023-06-13 13:13:24 -0400

    set number of exercises per page

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -5,7 +5,19 @@ import { exerciseOptions, fetchData} from '../utils/fetchData';
 import ExerciseCard from './ExerciseCard';
 
 const Exercises = ({ exercises, setExercises, bodyPart}) => {
-  console.log(exercises);
+  const [currentPage, setCurrentPage ] = useState(1);
+  const exercisesPerPage = 9;
+
+  const indexOfLastExercise = currentPage * exercisesPerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+  const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise);
+
+  const paginate = (e, value) => {
+    setCurrentPage(value);
+
+    window.scrollTo({ top: 1800, behavior: 'smooth'})
+  }
+
   return (
     <Box id="exercises"
       sx={{mt: {lg: '110px'}}}
@@ -20,8 +32,18 @@ const Exercises = ({ exercises, setExercises, bodyPart}) => {
           <ExerciseCard key={index} exercise={exercise} />
         ))}
       </Stack>
-      <Stack>
-        
+      <Stack mt="100px" alignItems="center">
+          {exercises.length > 9 &&  (
+            <Pagination
+              color="standard"
+              shape="rounded"
+              defaultPage={1}
+              count={Math.ceil(exercises.length / exercisesPerPage)}
+              page={currentPage}
+              onChange={paginate}
+              size="large"
+            />
+          )}
       </Stack>
     </Box>
   )
